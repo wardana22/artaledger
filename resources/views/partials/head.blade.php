985adfff2ca5304e14d39d9c7c507b8d627735a1
wardana22 <wardana.bayusy@gmail.com>
2026-09-06 17:30:17 +0700

    fix(branding): replace default laravel favicon with dynamic company logo

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -4,15 +4,16 @@
 @php
     $headCompany = \App\Models\Company::first();
     $headAppName = $headCompany?->app_name ?? config('app.name', 'E-counting');
+    $headFaviconUrl = $headCompany?->logo_url ?? asset('favicon.png');
 @endphp
 
 <title>
     {{ filled($title ?? null) ? $title.' - '.$headAppName : $headAppName }}
 </title>
 
-<link rel="icon" href="/favicon.ico" sizes="any">
-<link rel="icon" href="/favicon.svg" type="image/svg+xml">
-<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+<link rel="icon" href="{{ $headFaviconUrl }}">
+<link rel="shortcut icon" href="{{ $headFaviconUrl }}">
+<link rel="apple-touch-icon" href="{{ $headFaviconUrl }}">
 
 @fonts
 
